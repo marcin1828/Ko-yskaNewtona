535032b18f40308368c7e7952eb36115837800c4
Michal16511 <michal16511@gmail.com>
2019-01-18 00:33:31 +0100

    Junit5 setup

---
+++ CounterTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CounterTest {
+
+    @Test
+    void rhsTest() {
+        assertTrue(5 > 1);
+    }
+
+    @Test
+    void rungeTest() {
+    }
+
+    @Test
+    void nextDegreeTest() {
+    }
+}
