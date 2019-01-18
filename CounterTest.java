9e78cc879d344471ff3f03ea4dfdb3b7b0ac589e
Michal16511 <michal16511@gmail.com>
2019-01-18 01:16:14 +0100

    test for rhs method added

--- CounterTest.java
+++ CounterTest.java
@@ -4,9 +4,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CounterTest {
 
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Test
+    public void addTest() {
+        assertTrue(add(2, 2) == 4);
+    }
+
     @Test
     void rhsTest() {
-        assertTrue(5 > 1);
+        Counter counter = new Counter();
+        double inputArray[] = {2, 2};
+        double arrayResult[] = {2, -8.92021};
+
+        double[] returnedFromRhsArray = counter.rhs(inputArray);
+        assertAll("nnsad",
+                () -> assertEquals(arrayResult[0], returnedFromRhsArray[0], 0.05),
+                () -> assertEquals(arrayResult[1], returnedFromRhsArray[1], 0.05));
     }
 
     @Test
---
+++ PendulumTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PendulumTest {
+
+    @Test
+    void checkCollisionTest() {
+
+        assertTrue(3 > 0);
+    }
+}
